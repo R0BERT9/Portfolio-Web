6fbbd53c4d99731effdb9a528b0ac650d604cfca
ROBERT9 <robertocarlos.perez141@gmail.com>
2024-05-20 18:42:45 -0500

    Portfolio Update

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing', {
+    strings: ['Roberto', 'Frontend', 'Programmer'],
+    typeSpeed: 75,
+    backSpeed:70,
+    loop:true
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'aeonik':["DM Sans", 'sans-serif'],        
+      }
+    },
+  },
+  plugins: [],
+}
+
